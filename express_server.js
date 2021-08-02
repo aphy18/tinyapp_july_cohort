ba14d22cee0a561824b3af6014c59859ae1a4a0e
aphy18 <aphason1@.com>
2021-08-02 21:47:57 +0000

    connected to port 5000

---
+++ express_server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+const urlDatabase = {
+
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get('/', (req,res) => {
+  res.send("Hello");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+
+
+
